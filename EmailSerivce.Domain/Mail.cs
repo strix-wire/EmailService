f4175e638e927d1043d89445f9de9067aed01f50
Vasily <vasily_pavlov_98@mail.ru>
2022-10-01 19:41:51 +0700

    Create domen Mail.cs

---
+++ EmailSerivce.Domain/Mail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailSerivce.Domain
+{
+    public class Mail
+    {
+        public string FromName { get; set; }
+        public string FromAddress { get; set; }
+        public string ToName { get; set; }
+        public string ToAddress { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
